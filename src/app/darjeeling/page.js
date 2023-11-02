867285152d7f72372fd7ad6f44702d8107368940
Riktam Santra <riktamsantra56@gmail.com>
2023-11-02 16:09:47 +0530

    integrated weather API

---
+++ src/app/darjeeling/page.js
@@ -0,0 +1,47 @@
+import hill from './../assets/img/hill-black.svg'
+import './../style/darjeeling/index.css'
+import Image from 'next/image'
+import weather from './../assets/img/rainy.png'
+
+const getWeather = async () => {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=27.0380421&lon=88.262847&units=metric&appid=772d63bf896d0e4db07e363ba179e12c")
+    const responseString= await response.text();
+    const parsedJson = JSON.parse(responseString);
+    return parsedJson;
+  }
+  
+  const comp = getWeather().then((jsonData) => {
+    return <div className={"weather"}>
+      <div class="card">
+                <div class="card-body">
+                    <div className='card-icon'>
+                        <Image src={weather} height={50}/>
+                    </div>
+                    <div className='card-text'>
+                        <h5 class="card-title">{jsonData.main.temp} °C</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Rainy</h6>
+                    </div>
+                </div>
+            </div>
+    </div>
+  });
+  
+
+export default function Darjeeling() {
+    return (
+        <div className='container-fluid'>
+            {/* This card For Wather */}
+            {comp}
+            {/* <div className='hero-group'> */}
+            <div className='main-text'>
+                <h2>DARJEELING</h2>
+            </div>
+            <div className='main-img'>
+                <Image src={hill} className='hill-img' width={200} height={200} />
+            </div>
+            {/* </div> */}
+
+        </div>
+    )
+}
+
