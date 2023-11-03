73be60514af9bf4058d4126dbf9f96e5bcb6aec5
badmansp420 <sarkarprotik240@gmail.com>
2023-11-03 12:32:18 +0530

    Added map and user area

--- src/app/darjeeling/page.js
+++ src/app/darjeeling/page.js
@@ -6,6 +6,10 @@ import Time from './../assets/img/clock.png'
 import Airport from './../assets/img/airport.png'
 import Train from './../assets/img/train.png'
 import Kanchanjangha from './../assets/img/kanchanjangha.jpg'
+import Hospital from './../assets/img/hospital.png'
+import Transport from './../assets/img/vehicles.png'
+import Store from './../assets/img/store.png'
+import Restaurant from './../assets/img/restaurant.png'
 
 import iconMap from '../consts'
 import { log } from 'console'
@@ -48,153 +52,194 @@ const comp = getWeather().then((jsonData) => {
 
 export default function Darjeeling() {
     return (
-        <div className='container-fluid'>
-            <div className='hero'>
-                {/* This card For Wather */}
-                <div className='left-group'>
-                    {comp}
-                </div>
-                {/* This div for right side group */}
-                <div className='right-group'>
-                    {/* for  timing to visit*/}
-                    <div className={"distance-airport"}>
-                        <div className="card">
-                            <div class="card-body">
-                                <div className='card-icon'>
-                                    <Image src={Airport} className='card-icon-img' />
-                                </div>
-                                <div className="card-text">
-                                    <h5 className="card-title">DISTANCE FROM AIRPORT</h5>
-                                    <h6 className="card-subtitle mb-2 text-body-secondary">
-                                        30 KMs</h6>
-                                </div>
-                            </div>
+        <html lang='en'>
+            <head>
+                <title>Darjeeling</title>
+            </head>
+            <body>
+                <div className='container-fluid'>
+                    <div className='hero'>
+                        {/* This card For Wather */}
+                        <div className='left-group'>
+                            {comp}
                         </div>
-                    </div>
-                    {/* for  timing to visit*/}
-                    <div className={"distance-railway"}>
-                        <div className="card">
-                            <div class="card-body">
-                                <div className='card-icon'>
-                                    <Image src={Train} className='card-icon-img' />
-                                </div>
-                                <div className="card-text">
-                                    <h5 className="card-title">DISTANCE FROM RAILWAY</h5>
-                                    <h6 className="card-subtitle mb-2 text-body-secondary">
-                                        30 KMs</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* for  timing to visit*/}
-                    <div className={"visit-timing"}>
-                        <div className="card">
-                            <div class="card-body">
-                                <div className='card-icon'>
-                                    <Image src={Time} className='card-icon-img' />
-                                </div>
-                                <div className="card-text">
-                                    <h5 className="card-title">BEST TIME TO VISIT</h5>
-                                    <h6 className="card-subtitle mb-2 text-body-secondary">
-                                        APR - AUG</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="main-text">
-                    <h2>DARJEELING</h2>
-                </div>
-                <div className='main-img'>
-                    <Image src={hill} className='hill-img' />
-                </div>
-
-
-            </div>
-
-            <article className='container'>
-                <div className='body-hill'>
-                    <div className='card'>
-                        <div class="card-body">
-                            <div className='card-icon'>
-                                <Image src={Kanchanjangha} className='card-img-left' />
-                            </div>
-                            <div className='card-text'>
-                                <h5 className="card-title"> Lorem Ipsum</h5>
-                                <h6 className="card-subtitle mb-2 text-body-secondary">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore
-                                    magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip ex ea
-                                    commodo consequat. Duis aute irure dolor in reprehenderit
-                                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                                    qui officia deserunt mollit anim id est laborum.
-                                </h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* TOP PLACES TO VISIT */}
-                <div className='top-place'>
-                    <div className="card">
-                        <div className="card-header">
-                            TOP PLACES TO VISIT
-                        </div>
-                        <div className="card-body">
-                            {/* inside card */}
-                            <div className='ghoom'>
-                                <div class="card inside-card ">
-                                    <Image src={Kanchanjangha} class="card-img-top" alt="..." />
-                                    <div class="card-body-inside">
-                                        <h5 class="card-title">BATASHIA LOOP</h5>
-                                        <p class="card-text">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                            sed do eiusmod tempor incididunt ut labore et dolore magna
-                                            aliqua. Dui id ornare arcu odio ut sem.
-                                        </p>
+                        {/* This div for right side group */}
+                        <div className='right-group'>
+                            {/* for  timing to visit*/}
+                            <div className={"distance-airport"}>
+                                <div className="card">
+                                    <div class="card-body">
+                                        <div className='card-icon'>
+                                            <Image src={Airport} className='card-icon-img' />
+                                        </div>
+                                        <div className="card-text">
+                                            <h5 className="card-title">DISTANCE FROM AIRPORT</h5>
+                                            <h6 className="card-subtitle mb-2 text-body-secondary">
+                                                30 KMs</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className='ghoom'>
-                                <div class="card inside-card ">
-                                    <Image src={Kanchanjangha} class="card-img-top" alt="..." />
-                                    <div class="card-body-inside">
-                                        <h5 class="card-title">GHOOM</h5>
-                                        <p class="card-text">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                            sed do eiusmod tempor incididunt ut labore et dolore magna
-                                            aliqua. Dui id ornare arcu odio ut sem.
-                                        </p>
+                            {/* for  timing to visit*/}
+                            <div className={"distance-railway"}>
+                                <div className="card">
+                                    <div class="card-body">
+                                        <div className='card-icon'>
+                                            <Image src={Train} className='card-icon-img' />
+                                        </div>
+                                        <div className="card-text">
+                                            <h5 className="card-title">DISTANCE FROM RAILWAY</h5>
+                                            <h6 className="card-subtitle mb-2 text-body-secondary">
+                                                30 KMs</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className='ghoom'>
-                                <div class="card inside-card ">
-                                    <Image src={Kanchanjangha} class="card-img-top" alt="..." />
-                                    <div class="card-body-inside">
-                                        <h5 class="card-title">TIGER HILL</h5>
-                                        <p class="card-text">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                            sed do eiusmod tempor incididunt ut labore et dolore magna
-                                            aliqua. Dui id ornare arcu odio ut sem.
-                                        </p>
+                            {/* for  timing to visit*/}
+                            <div className={"visit-timing"}>
+                                <div className="card">
+                                    <div class="card-body">
+                                        <div className='card-icon'>
+                                            <Image src={Time} className='card-icon-img' />
+                                        </div>
+                                        <div className="card-text">
+                                            <h5 className="card-title">BEST TIME TO VISIT</h5>
+                                            <h6 className="card-subtitle mb-2 text-body-secondary">
+                                                APR - AUG</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="card-footer  text-center">
-                            <button type='button' className='btn btn-outline-secondary'> See More </button>
+                        <div className="main-text">
+                            <h2>DARJEELING</h2>
                         </div>
+                        <div className='main-img'>
+                            <Image src={hill} className='hill-img' />
+                        </div>
+
+
                     </div>
-                </div>
 
+                    <article className='container'>
+                        <div className='body-hill'>
+                            <div className='card'>
+                                <div class="card-body">
+                                    <div className='card-icon'>
+                                        <Image src={Kanchanjangha} className='card-img-left' />
+                                    </div>
+                                    <div className='card-text'>
+                                        <h5 className="card-title"> Lorem Ipsum</h5>
+                                        <h6 className="card-subtitle mb-2 text-body-secondary">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                            sed do eiusmod tempor incididunt ut labore et dolore
+                                            magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                            exercitation ullamco laboris nisi ut aliquip ex ea
+                                            commodo consequat. Duis aute irure dolor in reprehenderit
+                                            in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                            Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                                            qui officia deserunt mollit anim id est laborum.
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* TOP PLACES TO VISIT */}
+                        <div className='top-place'>
+                            <div className="card">
+                                <div className="card-header">
+                                    TOP PLACES TO VISIT
+                                </div>
+                                <div className="card-body">
+                                    {/* inside card */}
+                                    <div className='ghoom'>
+                                        <div class="card inside-card ">
+                                            <Image src={Kanchanjangha} class="card-img-top" alt="..." />
+                                            <div class="card-body-inside">
+                                                <h5 class="card-title">BATASHIA LOOP</h5>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                                                    aliqua. Dui id ornare arcu odio ut sem.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='ghoom'>
+                                        <div class="card inside-card ">
+                                            <Image src={Kanchanjangha} class="card-img-top" alt="..." />
+                                            <div class="card-body-inside">
+                                                <h5 class="card-title">GHOOM</h5>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                                                    aliqua. Dui id ornare arcu odio ut sem.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='ghoom'>
+                                        <div class="card inside-card ">
+                                            <Image src={Kanchanjangha} class="card-img-top" alt="..." />
+                                            <div class="card-body-inside">
+                                                <h5 class="card-title">TIGER HILL</h5>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                                                    aliqua. Dui id ornare arcu odio ut sem.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card-footer  text-center">
+                                    <button type='button' className='btn btn-outline-secondary'> See More </button>
+                                </div>
+                            </div>
+                        </div>
 
-            </article >
+                        <div className='map-area'>
+                            <div className="card">
+                                <div className="card-header">
+                                    SERVICES NEARBY
+                                </div>
+                                <div className="card-body">
+                                    {/* inside card  for map view*/}
+                                    <iframe src="https://maps.google.com/maps?q=darjeeling%20Dates%20Products&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" allowfullscreen />
 
-            {/* for body */}
+                                    <div className='hospital'>
+                                        <Image src={Hospital} className='over-map-icon' />
+                                    </div>
+                                    <div className='transport'>
+                                        <Image src={Transport} className='over-map-icon' />
+                                    </div>
+                                    <div className='popular-store'>
+                                        <Image src={Store} className='over-map-icon' />
+                                    </div>
+                                    <div className='restaurent'>
+                                        <Image src={Restaurant} className='over-map-icon' />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='user'>
+                            <div className="card">
+                                <div className="card-header">
+                                    USER EXPERIENCES
+                                </div>
+                                <div className="card-body">
+                                    {/* inside card  for map view*/}
 
-        </div >
+                                </div>
+                            </div>
+                        </div>
+                    </article >
+
+                    {/* for body */}
+
+                </div >
+            </body >
+        </html >
     )
 }
 
